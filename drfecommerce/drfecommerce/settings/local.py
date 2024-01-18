14677b013e9302c368b14f0cc8abac8837a61496
tanzin706 <61379151+tanzin706@users.noreply.github.com>
2024-01-18 17:27:57 +0600

    2nd commit

--- drfecommerce/drfecommerce/settings/local.py
+++ drfecommerce/drfecommerce/settings/local.py
@@ -5,8 +5,8 @@ from drfecommerce.settings.base import *
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
