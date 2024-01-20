59653597e4a1f2bf4d96c1300194abe002ae9831
tanzin706 <61379151+tanzin706@users.noreply.github.com>
2024-01-20 17:17:46 +0600

    7th folder 3rd video done

--- drfecommerce/drfecommerce/product/models.py
+++ drfecommerce/drfecommerce/product/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from mptt.models import MPTTModel, TreeForeignKey
+
 
 # Create your models here.
+class Category(MPTTModel):
+    name = models.CharField(max_length=100)
+    parent = TreeForeignKey(
+        "self", on_delete=models.PROTECT, null=True, blank=True
+    )  # if you want to delete category, then u first have to delete all the entries of it first...example: want to del clothes(u 1st need to del shirts and pants)
+
+    class MPTTMeta:
+        order_insertion_by = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    is_digital = models.BooleanField(default=False)
+    brand = models.ForeignKey(
+        Brand, on_delete=models.CASCADE
+    )  # if brand is deleted all products of that brand will be deleted
+    Category = TreeForeignKey(
+        "Category", on_delete=models.SET_NULL, null=True, blank=True
+    )
+
+    def __str__(self):
+        return self.name
--- drfecommerce/drfecommerce/settings/base.py
+++ drfecommerce/drfecommerce/settings/base.py
@@ -45,6 +45,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # External Packages
+    "rest_framework",
+    # Internatl Apps
+    "drfecommerce.product",
 ]
 
 MIDDLEWARE = [
@@ -118,3 +122,4 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+REST_FRAMEWORK = {}
