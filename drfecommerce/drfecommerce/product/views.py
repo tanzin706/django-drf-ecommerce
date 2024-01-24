76b13f2053b62456782f9df74ad164509c669346
tanzin706 <61379151+tanzin706@users.noreply.github.com>
2024-01-24 11:58:05 +0600

    jan-24-1st-commit

--- drfecommerce/drfecommerce/product/views.py
+++ drfecommerce/drfecommerce/product/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
 
 # Create your views here.
+
+from .models import Category
+from .serializers import CategorySerializer
+
+
+class CategoryViewSet(viewsets.ViewSet):
+    """
+    A Simple Viewset for viewing all Categoreis
+    """
+
+    queryset = Category.objects.all()  # database to django(queryset)
+
+    def list(self, request):
+        serializer = CategorySerializer(
+            self.queryset, many=True
+        )  # django to serializer
+        return Response(serializer.data)  # serializer to response
--- drfecommerce/drfecommerce/settings/base.py
+++ drfecommerce/drfecommerce/settings/base.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # External Packages
     "rest_framework",
+    "drf_spectacular",
     # Internatl Apps
     "drfecommerce.product",
 ]
@@ -122,4 +123,10 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-REST_FRAMEWORK = {}
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Django DRF Ecommerce",
+}
